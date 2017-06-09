c12ed734d4e79d483d50f96812e38b9bfe08be48
Federico Locci <f.locci@entando.com>
2017-06-09 15:15:37 +0200

    Insert login system

--- src/main/java/org/entando/entando/tool/migration/Migrate.java
+++ src/main/java/org/entando/entando/tool/migration/Migrate.java
@@ -15,6 +15,8 @@ package org.entando.entando.tool.migration;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -22,42 +24,44 @@ import java.util.List;
  */
 public class Migrate {
 
-    private final List<ISQLMigration> migrate;
+	private final List<ISQLMigration> migrate;
 
-    Migrate() {
-        this.migrate = new ArrayList();
-    }
+	private static final Logger LOGGER = LoggerFactory.getLogger(Migrate.class);
 
-    public void addMigration(final ISQLMigration sqlMigration) {
-        migrate.add(sqlMigration);
-    }
+	Migrate() {
+		this.migrate = new ArrayList();
+	}
 
-    public void run() {
-        migrate.forEach(action -> {
-            action.execute();
-        });
-    }
+	public void addMigration(final ISQLMigration sqlMigration) {
+		migrate.add(sqlMigration);
+	}
 
-    public static void main(String[] args) {
-        System.out.println("Starting migration");
-        Options.loadParams(args);
-        Migrate migrate = new Migrate();
+	public void run() {
+		migrate.forEach(action -> {
+			action.execute();
+		});
+	}
 
-        final DatabaseDriver.SUPPORTED_DBMS driver = Options.getJdbcDriver().equals("postgresql")
-                ? DatabaseDriver.SUPPORTED_DBMS.POSTGRESQL : DatabaseDriver.SUPPORTED_DBMS.MYSQL;
+	public static void main(String[] args) {
+		LOGGER.info("Starting migration");
+		Options.loadParams(args);
+		Migrate migrate = new Migrate();
 
-        final PooledConnection sourceDatabasePort = PooledConnectionFactory.getConnection(driver, Options.getSrcUrl(), Options.getUsername(), Options.getPassword());
+		final DatabaseDriver.SUPPORTED_DBMS driver = Options.getJdbcDriver().equals("postgresql")
+				? DatabaseDriver.SUPPORTED_DBMS.POSTGRESQL : DatabaseDriver.SUPPORTED_DBMS.MYSQL;
 
-        final String urlSourceDatabaseEnv = Options.getSrcUrl().substring(0, Options.getSrcUrl().lastIndexOf("Port")).concat("Serv");
-        final PooledConnection sourceDatabaseEnv = PooledConnectionFactory.getConnection(driver, urlSourceDatabaseEnv, Options.getUsername(), Options.getPassword());
+		final PooledConnection sourceDatabasePort = PooledConnectionFactory.getConnection(driver, Options.getSrcUrl(), Options.getUsername(), Options.getPassword());
 
-        final PooledConnection destinationDatabase = PooledConnectionFactory.getConnection(driver, Options.getDstUrl(), Options.getUsername(), Options.getPassword());
+		final String urlSourceDatabaseEnv = Options.getSrcUrl().substring(0, Options.getSrcUrl().lastIndexOf("Port")).concat("Serv");
+		final PooledConnection sourceDatabaseEnv = PooledConnectionFactory.getConnection(driver, urlSourceDatabaseEnv, Options.getUsername(), Options.getPassword());
 
-        migrate.addMigration(new SQLMigrationPagesLastUpdate(sourceDatabasePort, sourceDatabaseEnv, destinationDatabase));
-        migrate.run();
+		final PooledConnection destinationDatabase = PooledConnectionFactory.getConnection(driver, Options.getDstUrl(), Options.getUsername(), Options.getPassword());
 
-        System.out.println("done.");
-        System.exit(0);
+		migrate.addMigration(new SQLMigrationPagesLastUpdate(sourceDatabasePort, sourceDatabaseEnv, destinationDatabase));
+		migrate.run();
 
-    }
+		LOGGER.info("done.");
+		System.exit(0);
+
+	}
 }
